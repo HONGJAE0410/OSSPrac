0069525bcb03a35f0a5b72ec639b64a9d4e0155a
KIM HONGJAE <ghdwo0410@dgu.ac.kr>
2024-09-26 19:39:34 +0900

    M main.py plus grade

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 name = input("이름을 입력하세요 : ")
 id = input("학번을 입력하세요 : ") 
-major = input("전공을 입력하세요")
+major = input("전공을 입력하세요 : ")
+grade = input("학년을 입력하세요 : ")
 
 print("")
 print("<출력>")
-print(f" 이름 : {name}\n 학번 : {id}\n 전공 : {major}")
+print(f" 이름 : {name}\n 학번 : {id}\n 전공 : {major}\n 학년 : {grade}")
