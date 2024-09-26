e4fa83ce5a9ad1c02ec505614d043303a37820b6
KIM HONGJAE <ghdwo0410@dgu.ac.kr>
2024-09-26 16:09:10 +0900

    modify main.py plus id

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("Hello world~~")
+name = input("이름을 입력하세요 : ")
+id = input("학번을 입력하세요 : ") 
+major = input("전공을 입력하세요 : ")
+
+print("")
+print("<출력>")
+print(f" 이름 : {name}\n 학번 : {id}\n 전공 : {major}")
